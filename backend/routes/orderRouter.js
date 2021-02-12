63ce177d0d8592f5852478dd8959664541d92edd
nahimanajz <jazzonahimana@gmail.com>
2021-02-12 17:28:18 +0200

    if applied this commit will record an order payment

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -29,13 +29,27 @@ orderRouter.get('/:id',isAuth, expressAsyncHandler( async(req, res) => {
  return order ? res.send(order): res.status(404).send({message:'Order not Found'});
 }));
 orderRouter.get('/client/orders', isAuth, expressAsyncHandler( async(req, res)=> {
-  
-   try { 
         const orders = await Order.find({user: req.user._id});
         return orders ? res.status(200).send(orders) : res.status(404).send({message: 'Order not Found'});
-    
-    } catch (error) {
-        return res.status(500).send({error: error.message,message:'internal server error', user: req.user._id});
-    } 
+}));
+orderRouter.put('/:id/pay', isAuth, expressAsyncHandler( async(req, res)=> {
+    const order = await Order.findById(req.params.id);
+    console.log("payment route is visited");
+    if(order) {
+        const {id, status, update_time, email_address} = req.body;
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.status = status;
+        order.paymentResult = {
+            id,
+            status,
+            update_time,
+            email_address
+        };
+        const updatedOrder = await order.save();
+        return res.send({message: 'order is paid', order:updatedOrder});
+    } else {
+        return res.status(400).send({message: 'Order not Found'});
+    }
 }))
 export default orderRouter;
